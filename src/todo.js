4bf262e50ddb985c1c949b68930a60affc5b8ee0
mbosenisum <mbose@nisum.com>
2024-01-11 00:16:11 -0800

    added removeTodo

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,14 @@
 // https://www.theodinproject.com/lessons/node-path-javascript-factory-functions-and-the-module-pattern
 
+// current: 
+// tie all functionality to DOM
+// extra individual todos (within each project) after viewing all 
+// expand a single todo to see/edit its deatils
+// delete a todo
+// -- localStorage
+
+
+
 function Todo(title, description, dueDate, priority, notes, checklist) {
     this.title = title;
     this.description = description;
@@ -10,8 +19,6 @@ function Todo(title, description, dueDate, priority, notes, checklist) {
     return { title, description, dueDate, priority, notes, checklist }
 }
 
-// make a private variable
-// allProjects = [];
 function projectGroup (newProject) {
     let arr = [];
 
@@ -29,42 +36,68 @@ function projectGroup (newProject) {
 
 let allProjects = projectGroup();
 
-// as factory function
 function Project(newTodo) {
     let todos = [];
 
     todos.push(newTodo);
     allProjects.add(newTodo);
 
-    // not sure if 'function' is needed or not
     const addTodo = (newTodo) => {
         todos.push(newTodo);
         return;
-        // allProjects.push(newTodo)
     }
 
+    // just returns todos
     const showTodos = () => {
-        // for(let todo in todos){
-        //     return todo;
-        // }
-        return todos;
+        for(let index in todos){
+            console.log(todos[index]);
+            // print to DOM
+        }
+        // return todos;
     }
 
-    return { todos, addTodo, showTodos };
+    const findTodo = (newTodo) => {
+        // iterate through Project
+        for(let index in todos){
+            console.log('title = ' + todos[index].title);
+            // if(todos[todo].title == newTodo.title){
+            //     console.log('titles match');
+            // }
+            if(todos[index] == newTodo){
+                console.log('todos match');
+                // break;  // do something else
+                return index;
+            }
+        }
+        return;
+    }
+
+    const removeTodo = (newTodo) => {
+        // search function
+
+        // console.log('old todo');
+        // console.log(todos);
+
+        let index = findTodo(newTodo);
+
+
+        todos = todos.splice(index, 1);
+
+        // console.log('changed todo');
+        // console.log(todos);
+        return;
+    }
+
+    return { todos, addTodo, showTodos, removeTodo, findTodo };
 }
 
 
 const groceries = Todo("groceries", "butter, milk, eggs", "01/05/2023", "high", "use coupons", "a, b");
 const groceries2 = Todo("groceries2", "lettuce", "01/05/2023", "high", "use coupons", "a, b");
 
-// console.log(groceries);
-
-// let groceries
-// let groceriesproject = Project(groceries);
 let groceriesproject = Project(groceries);
 
 groceriesproject.addTodo(groceries2);
-// console.log(groceriesproject.todos);
 
 // second project to test DisplayProjects
 const bills = Todo("bills", "utility", "01/06/2023", "high", "pay by phone", "a, b");
@@ -73,6 +106,10 @@ const bills2 = Todo("bills", "rent", "01/06/2023", "high", "pay by phone", "a, b
 let billsproject = Project(bills);
 billsproject.addTodo(bills2);
 
+console.log('finding todo');
+billsproject.removeTodo(bills);
+
+allProjects.add(billsproject);
 // insert function to print to DOM
 let content = document.getElementsByClassName('content');
 
@@ -87,3 +124,4 @@ function displayProjects() {
 let displayProjs = document.getElementById('displayProjects');
 
 displayProjs.addEventListener('click', displayProjects);
+
